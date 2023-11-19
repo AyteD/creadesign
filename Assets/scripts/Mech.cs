c3750777a86efdd4385d57508826d4a53c450352
AyteD <85117089+AyteD@users.noreply.github.com>
2023-11-19 18:22:21 +0100

    2.2

--- Assets/scripts/Mech.cs
+++ Assets/scripts/Mech.cs
@@ -1,24 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+//Utiliser le namespace AI pour que le NavMeshAgent soit accessible !
 using UnityEngine.AI;
 
-public class Mech : MonoBehaviour
+public class AgentMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private NavMeshAgent agent;
+    public Transform destinationTransform;
+    NavMeshAgent agent;
 
-    void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(Vector3.zero);
-
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = destinationTransform.position;
     }
 }
+
---
+++ Assets/scripts/spawn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.UI.Image;
+using UnityEngine.UIElements;
+
+public class spawn : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Instantiate(original, position, rotation, parent);
+    }
+}
